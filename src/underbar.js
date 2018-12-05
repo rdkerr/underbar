71e5670328b2f231cc8bd1622c8e46a2065000eb
rdkerr <rogerdkerr@gmail.com>
2018-12-04 20:15:29 -0700

    Finished some

--- src/underbar.js
+++ src/underbar.js
@@ -208,6 +208,14 @@
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    iterator = iterator === undefined ? _.identity : iterator;
+    return !_.every(collection, function(item) {
+      if (iterator(item)) {
+        return false;
+      } {
+        return true;
+      }
+    });
   };
 
 
